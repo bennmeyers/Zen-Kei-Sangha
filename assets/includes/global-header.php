e582237eeb68f412bf75c5f756a37d2fe6bf0292
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-03-26 09:19:09 -0700

    changed font

--- assets/includes/global-header.php
+++ assets/includes/global-header.php
@@ -23,5 +23,6 @@
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
+    <link href="https://fonts.googleapis.com/css?family=Bubbler+One" rel="stylesheet">
 </head>
 <body>
