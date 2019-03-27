d1e730cb9fe92d32e9c1b6feedcd5f6d77f68686
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-03-26 17:36:36 -0700

    added facebook link

--- assets/includes/global-header.php
+++ assets/includes/global-header.php
@@ -24,5 +24,6 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
     <link href="https://fonts.googleapis.com/css?family=Bubbler+One" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 <body>
--- assets/includes/main-footer.php
+++ assets/includes/main-footer.php
@@ -2,4 +2,9 @@
 /*
 BEST PRACTICE: A copyright with a year range protects content.
 */
-?><footer>&copy; 2010-<?php echo date("Y"); ?></footer>
+?>
+<footer>
+    <a href="http://www.paypal.me/zenkeisangha">Donate to us!</a>
+    <div class="social"><a href="https://www.facebook.com/groups/169278456552524/" target="blank"><i class="fab fa-facebook"></i></a></div>
+    <div id="copyright">&copy; 2012-<?php echo date("Y"); ?></div>
+</footer>
