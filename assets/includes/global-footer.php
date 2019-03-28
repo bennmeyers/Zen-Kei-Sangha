313b5d4917850fc0847d8528d0738defce533853
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-03-28 16:23:31 -0700

    added google tracking

--- assets/includes/global-footer.php
+++ assets/includes/global-footer.php
@@ -6,5 +6,15 @@ BEST PRACTICE: Include Javascripts after the content has been loaded.
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
 <script src="assets/js/custom-shared.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-137268338-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-137268338-1');
+</script>
+
 </body>
 </html>
