3e96f4919f22f60b40bd00d2e82ab97c83d12ee8
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-03-28 17:08:03 -0700

    changed google icon to caendar icon

--- assets/includes/main-footer.php
+++ assets/includes/main-footer.php
@@ -5,6 +5,6 @@ BEST PRACTICE: A copyright with a year range protects content.
 ?>
 <footer>
     <a href="http://www.paypal.me/zenkeisangha">Donate to us!</a>
-    <div class="social"><a href="https://www.facebook.com/groups/169278456552524/" target="blank"><i class="fab fa-facebook"></i></a> <a href="https://twitter.com/ZenKeiSangha" target="blank"><i class="fab fa-twitter"></i></a> <a href="https://calendar.google.com/calendar?cid=emVua2Vpc2FuZ2hhQGdtYWlsLmNvbQ" target="blank"><i class="fab fa-google"></i></a></div>
+    <div class="social"><a href="https://www.facebook.com/groups/169278456552524/" target="blank"><i class="fab fa-facebook"></i></a> <a href="https://twitter.com/ZenKeiSangha" target="blank"><i class="fab fa-twitter"></i></a> <a href="https://calendar.google.com/calendar?cid=emVua2Vpc2FuZ2hhQGdtYWlsLmNvbQ" target="blank"><i class="fa fa-calendar" aria-hidden="true"></i></a></div>
     <div id="copyright">&copy; 2012-<?php echo date("Y"); ?></div>
 </footer>
