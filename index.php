87569732c8b957776bd868800ae483a4f2bae5b5
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-04-03 09:09:12 -0700

    added address anchor tags

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Including partial files most Content Management Systems (CMSs) divide out
+*/
+include("assets/includes/global-header.php"); // HTML head ?>
+
+<div id="homepage" class="container">
+  <div id="content">
+    <div id="main_content">
+      <h1>Zen Kei Sangha</h1>
+      <a href="http://www.paypal.me/zenkeisangha"><img src="/assets/img/logo.gif" alt="Zen Kei Sangha Logo"></a>
+      <p>Zen Kei Sangha is a collection of individuals forming a Sangha with the intention of learning about dharma and acting it out in a way which improves the lives of the homeless, those addicted to substances, or are not otherwise recognized in a society in which they don't feel comfortable. The Sangha offers three weekly public meditations, Harm Reduction Meditation, hospice support, and memorials for residents of Single Room Occupancies​ (SROs).</p>
+      <p>We specialize in <b>Harm Reduction</b> meditation.</p>
+
+      <hr />
+      We meet Mondays 6:30p-7:30p at Glide Community Center room 201 (Maya Angelou Room)
+      <address>
+        <a href="https://www.google.com/maps/place/333+Taylor+St,+San+Francisco,+CA+94102/@37.7854701,-122.4136567,17z/data=!3m1!4b1!4m5!3m4!1s0x8085808fbf13ac01:0xfa064570280bf978!8m2!3d37.7854701!4d-122.411468" target="_blank">333 Taylor Street
+        <br />San Francisco, CA 94102</a>
+      </address>
+      <hr />
+      You can also find us on Sunny Tuesdays and Thursdays 1:30p-2:30p in Howard & Langton Community Garden
+      <address>
+        <a href="https://www.google.com/maps/place/Howard+St+%26+Langton+St,+San+Francisco,+CA+94103/@37.7775491,-122.4121334,17z/data=!3m1!4b1!4m5!3m4!1s0x80858083007ff7c1:0x6f4fb388012d120a!8m2!3d37.7775491!4d-122.4099447" target="_blank">Howard St & Langton St
+        <br />San Francisco, CA 94103</a>
+      </address>
+      <hr />
+    </div><!-- #main_content -->
+  </div><!-- #content -->
+  <?php include("assets/includes/main-footer.php"); // Main foot ?>
+</div><!-- #homepage -->
+<?php include("assets/includes/global-footer.php"); // HTML foot ?>
