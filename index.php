e1c9844656f6ea1e90815b723b8e2fe650e85104
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-04-10 22:20:20 -0700

    added click tracking

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include("assets/includes/global-header.php"); // HTML head ?>
 <div id="homepage" class="container">
   <div id="content">
     <div id="main_content">
-      <a href="http://www.paypal.me/zenkeisangha"><img src="/assets/img/logo.gif" alt="Zen Kei Sangha Logo"></a>
+      <a href="http://www.paypal.me/zenkeisangha" onclick="ga('send','event','External','Link','PayPal');"><img src="/assets/img/logo.gif" alt="Zen Kei Sangha Logo"></a>
       <main>
         <p>Zen Kei Sangha is a collection of individuals forming a Sangha with the intention of learning about dharma and acting it out in a way which improves the lives of the homeless, those addicted to substances, or are not otherwise recognized in a society in which they don't feel comfortable. The Sangha offers three weekly public meditations, Harm Reduction Meditation, hospice support, and memorials for residents of Single Room Occupancies​ (SROs).</p>
         <p>We specialize in <b>Harm Reduction</b> meditation.</p>
@@ -22,7 +22,7 @@ include("assets/includes/global-header.php"); // HTML head ?>
         We meet Mondays 6:30p-7:30p at Glide Community Center room 201 (Maya Angelou Room)
       </p>
       <address>
-        <a href="https://www.google.com/maps/place/333+Taylor+St,+San+Francisco,+CA+94102/@37.7854701,-122.4136567,17z/data=!3m1!4b1!4m5!3m4!1s0x8085808fbf13ac01:0xfa064570280bf978!8m2!3d37.7854701!4d-122.411468" target="_blank">333 Taylor Street
+        <a href="https://www.google.com/maps/place/333+Taylor+St,+San+Francisco,+CA+94102/@37.7854701,-122.4136567,17z/data=!3m1!4b1!4m5!3m4!1s0x8085808fbf13ac01:0xfa064570280bf978!8m2!3d37.7854701!4d-122.411468" onclick="ga('send','location','External','Link','Glide');" target="_blank">333 Taylor Street
         <br />San Francisco, CA 94102</a>
       </address>
       <hr />
@@ -30,12 +30,12 @@ include("assets/includes/global-header.php"); // HTML head ?>
         You can also find us on Sunny Tuesdays and Thursdays 1:30p-2:30p in Howard & Langton Community Garden
       </p>
       <address>
-        <a href="https://www.google.com/maps/place/Howard+St+%26+Langton+St,+San+Francisco,+CA+94103/@37.7775491,-122.4121334,17z/data=!3m1!4b1!4m5!3m4!1s0x80858083007ff7c1:0x6f4fb388012d120a!8m2!3d37.7775491!4d-122.4099447" target="_blank">Howard St & Langton St
+        <a href="https://www.google.com/maps/place/Howard+St+%26+Langton+St,+San+Francisco,+CA+94103/@37.7775491,-122.4121334,17z/data=!3m1!4b1!4m5!3m4!1s0x80858083007ff7c1:0x6f4fb388012d120a!8m2!3d37.7775491!4d-122.4099447" onclick="ga('send','location','External','Link','Garden');" target="_blank">Howard St & Langton St
         <br />San Francisco, CA 94103</a>
       </address>
       <hr />
       <p>
-        We’re also happy to arrange new meditation groups: just <a href="mailto:zenkeisangha@gmail.com">contact us</a> for more information at <b>(415) 349-2491</b>.
+        We’re also happy to arrange new meditation groups: just <a href="mailto:zenkeisangha@gmail.com" onclick="ga('send','location','Internal','Link','Contact');">contact us</a> for more information at <b>(415) 349-2491</b>.
       </p>
       <hr />
     </div><!-- #main_content -->
