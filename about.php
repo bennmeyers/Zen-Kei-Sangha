16ed017c4848f1d935c7c9f24c5d5e780a44fd61
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-05-01 16:52:10 -0700

    added navigation and about page

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Including partial files most Content Management Systems (CMSs) divide out
+*/
+include("assets/includes/global-header.php"); // HTML head
+include("assets/includes/main-header.php"); // Main head ?>
+<div id="avbout" class="container">
+  <div id="content">
+    <div id="main_content">
+      <main>
+        <p>Zen Kei Sangha is a collection of individuals forming a Sangha with the intention of learning about dharma and acting it out in a way which improves the lives of the homeless, those addicted to substances, or are not otherwise recognized in a society in which they don't feel comfortable. The Sangha offers three weekly public meditations, Harm Reduction Meditation, hospice support, and memorials for residents of Single Room Occupancies​ (SROs).</p>
+        <p>We specialize in <b>Harm Reduction</b> meditation.</p>
+      </main>
+    </div><!-- #main_content -->
+  </div><!-- #content -->
+  <?php include("assets/includes/main-footer.php"); // Main foot ?>
+</div><!-- #homepage -->
+<?php include("assets/includes/global-footer.php"); // HTML foot ?>
--- assets/includes/main-header.php
+++ assets/includes/main-header.php
@@ -1,3 +1,6 @@
 <header>
   <h1><a href="index.php">Zen Kei Sangha</a></h1>
+  <ul class="nav navbar-nav">
+    <li><a href="about.php">about</a></li>
+  </ul>
 </header>
