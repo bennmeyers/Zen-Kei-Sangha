776d7d295b0a34e158260480953781159f6ce743
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-05-03 15:04:10 -0700

    added name request

--- contact.php
+++ contact.php
@@ -6,9 +6,10 @@ include("assets/includes/global-header.php"); // HTML head
 include("assets/includes/main-header.php"); // Main head ?>
   <div id="form" class="container">
     <?php
-      $to = 'zenkeisangha@gmail.com ';
+      $to = 'zenkeisangha@gmail.com';
+      $name = (isset($_POST['name'])) ? $_POST['name'] : '[Name Not Provided]';
       $subject = (isset($_POST['subject'])) ? $_POST['subject'] : 'Test Subject';
-      $message = (isset($_POST['body'])) ? $_POST['body'] : 'Test Question';
+      $message = (isset($_POST['body'])) ? $_POST['body'] . "from " . $name : 'Test Question';
       if (isset($_POST['from'])) {
         $headers = 'From: ' . $_POST['from'] . "\r\n" .
           'Reply-To: ' . $_POST['from'] . "\r\n" .
@@ -19,7 +20,14 @@ include("assets/includes/main-header.php"); // Main head ?>
 
       if(isset($_POST['submit'])) {
         mail($to, $subject, $message, $headers);
-      }
+        ?>
+      <div id="content">
+        <div id="main_content">
+          <h1 class="text-success">Thank You <?php echo $name; ?>! We'll get back to you soon!</h1>
+        </div><!-- #main_content -->
+      </div><!-- #content -->
+        <?php
+      } else {
     ?>
       <div id="content">
         <div id="main_content">
@@ -28,15 +36,19 @@ include("assets/includes/main-header.php"); // Main head ?>
             <form action="contact.php" method="POST">
               <div class="form-group">
                 <label for="subject">Subject</label>
-                <input type="text" class="form-control" placeholder="subject" id="subject">
+                <input type="text" class="form-control" placeholder="subject" id="subject" name="subject" required="required">
+              </div>
+              <div class="form-group">
+                <label for="subject">Name</label>
+                <input type="text" class="form-control" placeholder="your name" id="name" name="name">
               </div>
               <div class="form-group">
                 <label for="from">Email</label>
-                <input type="email" class="form-control" name="from" id="from" value="" placeholder="Your Email">
+                <input type="email" class="form-control" name="from" id="from" value="" placeholder="your email">
               </div>
               <div class="form-group">
                 <label for="body">Question</label>
-                <textarea class="form-control" rows="3" name="body" id="body" placeholder="Question"></textarea>
+                <textarea class="form-control" rows="3" name="body" id="body" placeholder="question"></textarea>
               </div>
               <input type="submit" name="submit" id="submit" value="submit" class="btn btn-default">
             </form>
@@ -44,8 +56,10 @@ include("assets/includes/main-header.php"); // Main head ?>
           <a href="mailto:zenkeisangha@gmail.com">zenkeisangha@gmail.com</a>
           <br /><b>415 349-3491</b>
         </div><!-- #main_content -->
-        <?php include("assets/includes/sub-nav.php"); ?>
       </div><!-- #content -->
+    <?php
+      }
+    ?>
     <?php include("assets/includes/main-footer.php"); ?>
   </div><!-- #form -->
 <?php include("assets/includes/global-footer.php"); // HTML foot ?>
