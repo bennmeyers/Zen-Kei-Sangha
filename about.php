c9fe433aa48fcccd0009cd6a9dda037ec3046b9c
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-05-04 09:48:55 -0700

    inserted about title

--- about.php
+++ about.php
@@ -7,6 +7,7 @@ include("assets/includes/main-header.php"); // Main head ?>
 <div id="about" class="container">
   <div id="content">
     <div id="main_content">
+      <h1>about</h1>
       <main>
         <p>Zen Kei Sangha is a collection of individuals forming a Sangha with the intention of learning about dharma and acting it out in a way which improves the lives of the homeless, those addicted to substances, or are not otherwise recognized in a society in which they don't feel comfortable. The Sangha offers three weekly public meditations, Harm Reduction Meditation, hospice support, and memorials for residents of Single Room Occupancies​ (SROs).</p>
         <p>We specialize in <b>Harm Reduction</b> meditation.</p>
