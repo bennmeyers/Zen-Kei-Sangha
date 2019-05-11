156a693b4977120593654a61a3f410571a037ca2
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-05-11 11:58:12 -0700

    formatted footer

--- assets/includes/main-footer.php
+++ assets/includes/main-footer.php
@@ -4,11 +4,19 @@ BEST PRACTICE: A copyright with a year range protects content.
 */
 ?>
 <footer>
-    <a class="btn btn-primary" href="http://www.paypal.me/zenkeisangha">Donate to us!</a>
-    <div class="social">
-        <a href="https://www.facebook.com/groups/169278456552524/" target="blank"><i class="fab fa-facebook"></i></a>
-        <a href="https://twitter.com/ZenKeiSangha" target="blank"><i class="fab fa-twitter"></i></a>
-        <a href="https://calendar.google.com/calendar?cid=emVua2Vpc2FuZ2hhQGdtYWlsLmNvbQ" target="blank"><i class="fa fa-calendar-alt" aria-hidden="true"></i></a>
+  <a class="btn btn-primary" href="http://www.paypal.me/zenkeisangha">Donate to us!</a>
+  <div class="row">
+    <div class="col-md-6">
+      <a href="mailto:zenkeisangha@gmail.com">zenkeisangha@gmail.com</a>
+      <br /><b>415 349-3491</b>
     </div>
-    <div id="copyright">&copy; 2012-<?php echo date("Y"); ?></div>
+    <div class="col-md-6">
+      <div class="social">
+          <a href="https://www.facebook.com/groups/169278456552524/" target="blank"><i class="fab fa-facebook"></i></a>
+          <a href="https://twitter.com/ZenKeiSangha" target="blank"><i class="fab fa-twitter"></i></a>
+          <a href="https://calendar.google.com/calendar?cid=emVua2Vpc2FuZ2hhQGdtYWlsLmNvbQ" target="blank"><i class="fa fa-calendar-alt" aria-hidden="true"></i></a>
+      </div>
+    </div>
+  </div>
+  <div id="copyright">&copy; 2012-<?php echo date("Y"); ?></div>
 </footer>
