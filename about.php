ae042dc286e25bf5768df0d4f080322120a3c391
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-05-15 23:28:22 -0700

    added a desciption of harm reduction

--- about.php
+++ about.php
@@ -11,6 +11,18 @@ include("assets/includes/main-header.php"); // Main head ?>
       <main>
         <p>Zen Kei Sangha is a collection of individuals forming a Sangha with the intention of learning about dharma and acting it out in a way which improves the lives of the homeless, those addicted to substances, or are not otherwise recognized in a society in which they don't feel comfortable. The Sangha offers three weekly public meditations, Harm Reduction Meditation, hospice support, and memorials for residents of Single Room Occupancies​ (SROs).</p>
         <p>We specialize in <b>Harm Reduction</b> meditation.</p>
+        <h3>Principles of Harm Reduction</h3>
+        <p>Harm reduction is a set of practical strategies and ideas aimed at reducing negative consequences associated with substance use. Harm Reduction is also a movement for social justice built on a belief in, and respect for, the rights of people who use drugs or alcohol.</p>
+        <p>Harm reduction incorporates a spectrum of strategies from safer use, to managed use to abstinence to meet people “where they’re at,” addressing conditions of use along with the use itself.</p>
+        <h3>Harm Reduction Practice</h3>
+        <p>Accepts, for better and or worse, that licit and illicit substance use is part of our world and chooses to work to minimize its harmful effects rather than simply ignore or condemn them.</p>
+        <p>Understands substance use as a complex, multifaceted phenomenon that encompasses a continuum of behaviors from severe abuse to total abstinence, and acknowledges that some ways of using substances are clearly safer than others.</p>
+        <p>Establishes quality of individual and community life and well-being–not necessarily cessation of all substance use–as the criteria for successful interventions and policies.</p>
+        <p>Calls for the non-judgmental, non-coercive provision of services and resources to people who use substances and the communities in which they live in order to assist them in reducing attendant harm.</p>
+        <p>Ensures that users and those with a history of use routinely have a real voice in the creation of programs and policies designed to serve them.</p>
+        <p>Affirms users themselves as the primary agents of reducing the harms of their use, and seeks to empower users to share information and support each other in strategies which meet their actual conditions of use.</p>
+        <p>Recognizes that the realities of poverty, class, racism, social isolation, past trauma, sex-based discrimination and other social inequalities affect both people’s vulnerability to and capacity for effectively dealing with substance-related harm.</p>
+        <p>Does not attempt to minimize or ignore the real and tragic harm and danger associated with licit and illicit substance use.</p>
       </main>
     </div><!-- #main_content -->
   </div><!-- #content -->
