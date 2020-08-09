2745975bc1126375bd53b07e31a65383d7a1df1b
Benjamin Meyers <benjamin@purespiritwebworks.com>
2020-08-09 16:18:32 -0700

    #49

--- resources.php
+++ resources.php
@@ -8,11 +8,25 @@ include("assets/includes/main-header.php"); // Main head ?>
   <div id="content">
     <div id="main_content">
       <h1>audio</h1>
-        <ul>
-          <li>https://www.youtube.com/watch?v=v6b44Q2Uexg Faithful Fools</li>
-          <li>https://www.youtube.com/watch?v=tGr-NYLjLOY Faithful Fools</li>
-          <li>https://www.youtube.com/watch?v=wSmQdwMI5lk Faithful Fools</li>
-        </ul>
+      <h2>faithful fools</h2>
+      <ul>
+        <li>https://www.youtube.com/watch?v=v6b44Q2Uexg</li>
+        <li>https://www.youtube.com/watch?v=tGr-NYLjLOY</li>
+        <li>https://www.youtube.com/watch?v=wSmQdwMI5lk</li>
+      </ul>
+      <h2>gay buddhist fellowship</h2>
+      <ul>
+        <li>2009, 07 29</li>
+        <li>2009, 08 30</li>
+        <li>2010, 08 29</li>
+        <li>2010, 09 05</li>
+        <li>2012, 04 15</li>
+        <li>2013, 02 03</li>
+        <li>2013, 09 15</li>
+        <li>2014, 04 30</li>
+        <li>2015, 05 17</li>
+        <li>2016, 04 17</li>
+      </ul>
       <h1>outside resources</h1>
       <p>The Zenkei Sangha is the next generation of Rev. Jana Drakka's Zenkei Zen Sangha, which continues the tradition of caring Jana established.
       <br><a href="https://web.archive.org/web/20111110094559/http://janadrakka.com/" target="_blank">Zenkei wayback machine archive</a></p>
