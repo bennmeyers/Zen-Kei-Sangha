eea9d7804e218b2815f86a2948470335a8b54e63
Benjamin Meyers <benjamin@purespiritwebworks.com>
2021-11-01 18:18:33 -0700

    the 2021 ceremony

---
+++ ceremony.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Including partial files most Content Management Systems (CMSs) divide out
+*/
+include("assets/includes/global-header.php"); // HTML head
+include("assets/includes/main-header.php"); // Main head ?>
+<div id="resources" class="container">
+  <div id="content">
+    <div id="main_content">
+      <h1>October 2021 Jukei and Okesa Ceremony</h1>
+      <video width="100%" controls>
+        <source src="assets/video/okesa-0-2-0-202110.mp4" type="video/mp4">
+        Your browser does not support the video tag. Contact Shishin for further assistance.
+      </video>
+    </div><!-- #main_content -->
+  </div><!-- #content -->
+  <?php include("assets/includes/main-footer.php"); // Main foot ?>
+</div><!-- #homepage -->
+<?php include("assets/includes/global-footer.php"); // HTML foot ?>
+</script>
